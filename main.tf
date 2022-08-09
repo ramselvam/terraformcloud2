d91821e28bd763f225fdcaf4d344fc64522c397b
ramesh <ramesh.selvaraj@neudesic.com>
2022-08-09 18:06:36 +0530

    third commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+data "azurerm_resource_group" "resource_group" {
+  for_each = var.network_security_group_variable
+  name     = each.value.resource_group_name
+}
+
+resource "azurerm_network_security_group" "network_security_group" {
+  for_each            = var.network_security_group_variable
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+  location            = data.azurerm_resource_group.resource_group[each.key].location
+  security_rule       = each.value.security_rule == null ? [] : each.value.security_rule
+  tags                = merge(each.value.network_security_group_tags, tomap({ Created_Time = formatdate("DD-MM-YYYY hh:mm:ss ZZZ", timestamp()) }))
+  lifecycle { ignore_changes = [tags["Created_Time"]] }
+}
